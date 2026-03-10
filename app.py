18c4766691f8d4fd350a28a5589ca52afe5d0846
Javier Medina <javiermedinag@hotmail.com>
2026-03-10 14:03:15 -0600

    Clase

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient #Librería de MongoDB para Python 
-from flask import Flask #Librería de Flask para crear aplicaciones web y renderizar plantillas HTML 
+from flask import Flask, request #Librería de Flask para crear aplicaciones web y renderizar plantillas HTML 
 app = Flask(__name__) #Creación de una instancia de la aplicación Flask  
 Cliente = MongoClient('mongodb://root:root@localhost:27017/') 
 #Conexión a la base de datos MongoDB utilizando el cliente de MongoDB. 
@@ -17,10 +17,18 @@ def index():
     return "Bienvenido a la Biblioteca", 200 #Función que se ejecuta cuando se accede a la URL raíz. Devuelve un mensaje de bienvenida y un código de estado HTTP 200 (OK).
 @app.route('/libros', methods=['GET']) #Endpoint de Flask que define una ruta para la URL "/libros" y especifica que se aceptan solicitudes GET.
 def libros():
-    libros_list = "<H1>Libros disponibles:</H1>\n"
-    for L in Lista:
-        libros_list += f"<p>{L['title']} - {L['author']}</p>\n" #Itera sobre la lista de libros obtenida de la base de datos y construye una cadena HTML que muestra el título y el autor de cada libro en un formato de párrafo.  
-    return libros_list, 200
+    if request.args.get('id'): #Verifica si se ha proporcionado un parámetro de consulta "id" en la solicitud GET. Si es así, se ejecuta el bloque de código dentro del if.
+        id = int(request.args.get('id')) #Obtiene el valor del parámetro de consulta "id" y lo asigna a la variable id.
+        libro = Libros.find_one({"id": id}) #Realiza una consulta a la colección "Libros" para encontrar un documento que tenga un campo "_id" igual al valor de id. El resultado se almacena en la variable libro.
+        if libro: #Verifica si se encontró un libro con el ID proporcionado. Si es así, se ejecuta el bloque de código dentro del if.
+            return f"<H1>{libro['title']} - {libro['author']}</H1>", 200 #Devuelve una cadena HTML que muestra el título y el autor del libro encontrado, junto con un código de estado HTTP 200 (OK).
+        else: #Si no se encontró ningún libro con el ID proporcionado, se ejecuta el bloque de código dentro del else.
+            return "Libro no encontrado", 404 #Devuelve un mensaje indicando que el libro no fue encontrado y un código de estado HTTP 404 (Not Found). 
+    else:
+        libros_list = "<H1>Libros disponibles:</H1>\n"
+        for L in Lista:
+            libros_list += f"<p>{L['title']} - {L['author']}</p>\n" #Itera sobre la lista de libros obtenida de la base de datos y construye una cadena HTML que muestra el título y el autor de cada libro en un formato de párrafo.  
+        return libros_list, 200
 
 if __name__ == '__main__': # Verifica si el script se está ejecutando directamente (en lugar de ser importado como un módulo) y, en ese caso, inicia la aplicación Flask.
     app.run(debug=True, host='0.0.0.0', port=5000) #Inicia la aplicación Flask en modo de depuración, lo que permite ver mensajes de error detallados en caso de que ocurra algún problema durante la ejecución de la aplicación.
